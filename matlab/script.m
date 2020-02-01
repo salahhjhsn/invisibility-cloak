3bbb41a24fdb9d2445f4feadc648c7577f99184d
salahhjhsn <salah.hjhsn@outlook.com>
2020-02-01 17:24:26 +0100

    Edit Matlab project; Final Version

--- matlab/script.m
+++ matlab/script.m
@@ -1,32 +1,42 @@
 cam = webcam(1);
-% cam = videoinput('macvideo',1);
+
 w = preview(cam);
 static_image = snapshot(cam);
+ 
+% define color here in hsv
+h = 0.66;
+s = 0.3;
+v = 0.3;
 
 while 1
     img = snapshot(cam);
+
+    hsv_img = rgb2hsv(img);
+
+    lower_h     = hsv_img(:,:,1)>(h-0.1);
+    lower_s     = hsv_img(:,:,2)>s;
+    lower_v     = hsv_img(:,:,3)>v;
+
+    upper_h     = hsv_img(:,:,1)<(h+0.1);
+
+    mask_h = lower_h.*upper_h;
+    mask_s = lower_s;
+    mask_v = lower_v;
     
-    r = round(mean(mean(img(:,:,1))));
+    mask = mask_h.*mask_s.*mask_v;
     
-    gray = rgb2gray(img);
-    bw = imbinarize(gray);
-    
-    red     = img(:,:,1)>180;
-    green   = img(:,:,2)<100;
-    blue    = img(:,:,3)<100;
-    
-    mask = red.*green.*blue;
-    
-    red_filter = uint8(mask).*img;
-    
-    cloak = img - red_filter + static_image.*uint8(mask);
-    
-%     imshow(img);
-%     imshow(gray);
-%     imshow(bw);
+    filter = uint8(mask).*img;
+
+    cloak = img - filter + static_image.*uint8(mask);
+
+%     imshow(filter);
 %     imshow(mask);
+%     imshow(mask_h);
+%     imshow(mask_s);
+%     imshow(mask_v);
+
     imshow(cloak);
-    
+
     if ishghandle(w) ~= 1
         break;
     end
@@ -34,4 +44,4 @@ end
 
 closePreview(cam);
 close all;
-clear cam;
+clear all;
