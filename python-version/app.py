4c978cc514300d6dd56a84f6f51401a468d9c73e
Ivan Turasov <ivan@thethingsindustries.com>
2020-02-02 21:19:06 +0100

    Add final python script

--- python-version/app.py
+++ python-version/app.py
@@ -5,67 +5,57 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 time.sleep(1)
-
-# define which color you want to mask out using the given plot of HSV. It's green here.
-lower_range = np.array([100, 40, 40])  # ([40, 30, 80])
-upper_range = np.array([100, 255, 255])  # ([45, 255, 255])
-
-
-# upper_red = np.array([100, 255, 255])
-
-time.sleep(1)
-count = 0
 background = 0
+background_timeout = 100
 
+input("Pleas get out of the camera FoV and press enter")
 
-
-for x in range(60):
+# Acquire the background image in the beginning, timeout to let the camera start up properly
+for x in range(background_timeout):
     ret, background = cap.read()
     if not ret:
         continue
 
+# First red mask ranges
+lower_red_one = np.array([0, 170, 70])  # 117, 234])
+upper_red_one = np.array([2, 226, 255])
+
+# Second red mask ranges, to avoid filtering augmented red colors
+lower_red_two = np.array([177, 170, 70])  # 117, 234])
+upper_red_two = np.array([180, 226, 255])
+
 while(cap.isOpened()):
-    return_val, img = cap.read()
-    if not return_val:
+    err, img = cap.read()
+    if not err:
         break
-    count = count + 1
-    img = np.flip(img, axis=1)
+    cv2.imshow("Original Image", img)
 
-    # convert the image - BGR to HSV
-    # as we focused on detection of red color
-
-    # converting BGR to HSV for better
-    # detection or you can convert it to gray
+    # BGR to HSV
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    #-------------------------------------BLOCK----------------------------#
-    # ranges should be carefully chosen
-    # setting the lower and upper range for mask1
-    lower_red = np.array([100, 40, 40])
-    upper_red = np.array([100, 255, 255])
-    mask1 = cv2.inRange(hsv, lower_red, upper_red)
-    # setting the lower and upper range for mask2
-    lower_red = np.array([155, 40, 40])
-    upper_red = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv, lower_red, upper_red)
-    #----------------------------------------------------------------------#
+    # Generate masks based on the defined thresholds
+    red_one = cv2.inRange(hsv, lower_red_one, upper_red_one)
+    red_two = cv2.inRange(hsv, lower_red_two, upper_red_two)
 
-    # the above block of code could be replaced with
-    # some other code depending upon the color of your cloth
-    mask1 = mask1 + mask2
+    red_one = red_one + red_two
 
-    # Refining the mask corresponding to the detected red color
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3),
-                                                            np.uint8), iterations=2)
-    mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations=1)
-    mask2 = cv2.bitwise_not(mask1)
+    # Creating the mask corresponding to the detected red color
+    red_one = cv2.morphologyEx(red_one, cv2.MORPH_OPEN, np.ones((3, 3),
+                                                                np.uint8))
+    red_two = cv2.morphologyEx(
+        red_one, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+    red_two = cv2.bitwise_not(red_one)
 
     # Generating the final output
-    res1 = cv2.bitwise_and(background, background, mask=mask1)
-    res2 = cv2.bitwise_and(img, img, mask=mask2)
-    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+    res1 = cv2.bitwise_and(background, background, mask=red_one)
+    # For testing the generated mask uncomment the next two lines
+    # res1 = cv2.bitwise_and(img, img, mask=red_one)
+    # cv2.imshow("Red Color Cloak!!!!", res1)
+    res2 = cv2.bitwise_and(img, img, mask=red_two)
 
-    cv2.imshow("INVISIBLE MAN", final_output)
+    output = cv2.addWeighted(res1, 1, res2, 1, 0)
+
+    cv2.imshow("Red Color Cloak", output)
     k = cv2.waitKey(10)
     if k == 27:
         break
